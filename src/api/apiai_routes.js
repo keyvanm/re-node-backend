61f86b2aa440896d8d3ff3b860f9839650632b63
Keyvan Mosharraf <Keyvan_Mosharraf@thecdm.ca>
2017-11-22 17:45:16 -0800

    refactor?

--- src/api/apiai_routes.js
+++ src/api/apiai_routes.js
@@ -1,51 +1,37 @@
-let API_AI = require('apiai');
-let { API_AI_KEY } = process.env;
-let apiai_client = API_AI(API_AI_KEY);
+const API_AI = require('apiai');
+const { API_AI_KEY } = process.env;
+const apiai_client = API_AI(API_AI_KEY);
 
-let randomize = (arr) => arr[Math.floor(Math.random() * arr.length)];
+const randomize = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-let sendResponse = ({ response, message }) => {
-    // console.log(message);
+const sendResponse = (response, message) => {
 	response.json(message);
 }
 
-let createTextMsg = (text) => {
-    return { messages: [{ text }] };
-}
+const createTextMsg = (text) =>  ({ messages: [{ text }] });
 
-let handleResponse = (response) => ({ result }) => {
-	let message;
+
+const sendQueryToApiAi = (req, res) => {
+  apiai_client.textRequest(req.body.queryString, {
+		sessionId: req.params.sessionId,
+		contexts: [{
+			name: req.body.context || 'DEFAULT',
+			parameters: req.body,
+		}]
+	}).on('response', ({ result }) => {
+		let message;
     if (result.source === 'agent') {
         let randomMsg = randomize(result.fulfillment.messages);
         message = randomMsg.payload ? randomMsg.payload : createTextMsg(randomMsg.speech);
     } else if (result.source === 'domains') {
         message = createTextMsg(result.fulfillment.speech);
     }
-
-    sendResponse({ response, message });
-}
-
-let handleError = (response) => (error) => {
-    console.log("handleError");
-    console.log(error);
-	let message = { error };
-	sendResponse({ response, message });
-}
-
-
-const sendQueryToApiAi = (req, res) => {
-	console.log(req.body);
-  let request = apiai_client.textRequest(req.body.queryString, {
-		sessionId: req.params.sessionId,
-		contexts: [{
-			name: req.body.context || 'DEFAULT',
-			parameters: req.body,
-		}]
-	});
-
-	request.on('response', handleResponse(res));
-	request.on('error', handleError(res));
-	request.end();
+		sendResponse(res, message);
+	}).on('error', (error) => {
+		console.log("handleError");
+    console.error(error);
+		sendResponse(res, error.message);
+	}).end();
 }
 
 export { sendQueryToApiAi };
